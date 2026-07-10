6622a235a9ea50ec02b8651a5fb0bbbef288f1a8
Prashant <prasantsingh945@gmail.com>
2026-07-10 14:39:15 +0530

    Day 40

---
+++ BST/searchInBST.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        while(root) {
+            int node = root->val;
+            if(node == val) return root;
+            if(node < val) root = root->right;
+            else root = root->left;
+        }
+        return NULL;
+    }
+};
